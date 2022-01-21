12e32cb19277ceb583af5262387ec6d0d8cdf1bd
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-01-21 19:30:02 +0100

    Menú amagat dispositius petits

--- vanilla/scripts.js
+++ vanilla/scripts.js
@@ -184,7 +184,20 @@ const configColor = () => {
 	});
 };
 
+// Amaga-ensenya menú
+const showMenu = () => {
+	$('#show-menu').addEventListener('click', () => {
+		$('#show-menu').classList.toggle('rot180');
+		$$('header div:not(#menu > div)').forEach((el) =>
+			el.style.display === '' || el.style.display === 'none'
+				? (el.style.display = 'block')
+				: (el.style.display = 'none')
+		);
+	});
+};
+
 // Crida funcions
 loadTexts();
 configMode();
 configColor();
+showMenu();
