a587026ad149867106e437f502ebf8d7b1223b4d
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-03-16 12:37:38 +0100

    Apartat formació

--- react/src/components/Education.js
+++ react/src/components/Education.js
@@ -10,6 +10,23 @@ const Education = () => {
 			{texts && (
 				<section id={texts.titles.menu[1]}>
 					<h3>{texts.titles.menu[1]}</h3>
+					{texts.education
+						.sort((course1, course2) => course1.year - course2.year)
+						.map((course, index) => (
+							<div key={`p-${index}`}>
+								<h4>{course.title}</h4>
+								<span key={`span-${index}`}>{course.school}</span> (
+								<a
+									key={`a-${index}`}
+									href={course.link.link}
+									target="_blank"
+									rel="noreferrer"
+								>
+									{course.link.text}
+								</a>
+								) - <span>{course.year}</span>
+							</div>
+						))}
 				</section>
 			)}
 		</>
