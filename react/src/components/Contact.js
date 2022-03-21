e0148be7132d5245f71bea4864aa1de56033e22c
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-03-21 17:24:25 +0100

    Apartat contacte

--- react/src/components/Contact.js
+++ react/src/components/Contact.js
@@ -1,15 +1,43 @@
 // react
 import { useContext } from 'react';
+import { FiDownload, FiMail, FiMapPin } from 'react-icons/fi';
 // contexts
 import DataContext from '../contexts/DataContext';
+import SettingsContext from '../contexts/SettingsContext';
 
 const Contact = () => {
-	const { texts } = useContext(DataContext);
+	const { texts, contact } = useContext(DataContext);
+	const { language } = useContext(SettingsContext);
+
 	return (
 		<>
 			{texts && (
 				<section id={texts.titles.menu[5]}>
 					<h3>{texts.titles.menu[5]}</h3>
+					<div className="info">
+						<span>
+							<FiMapPin />
+							<a
+								href="https://goo.gl/maps/GWAL3Gpnj6UGXJsQ8"
+								target="_blank"
+								rel="noreferrer"
+							>
+								{contact.location}
+							</a>
+						</span>
+						<span>
+							<FiMail />
+							<a href={`mailto:${contact.mail}`} target="_blank" rel="noreferrer">
+								{contact.mail}
+							</a>
+						</span>
+						<span>
+							<FiDownload />
+							<a href={`/cv_${language}.pdf`} target="_blank" rel="noreferrer">
+								pdf
+							</a>
+						</span>
+					</div>
 				</section>
 			)}
 		</>
