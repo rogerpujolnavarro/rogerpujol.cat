534b07d0645b564918df4720d9428cfdd35c3394
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-03-23 20:03:59 +0100

    Apartat projectes complet

--- react/src/components/Portfolio.js
+++ react/src/components/Portfolio.js
@@ -5,11 +5,39 @@ import DataContext from '../contexts/DataContext';
 
 const Portfolio = () => {
 	const { texts } = useContext(DataContext);
+	texts && console.log(texts.projects);
 	return (
 		<>
 			{texts && (
 				<section id={texts.titles.menu[3]}>
 					<h3>{texts.titles.menu[3]}</h3>
+					<div className="projects">
+						{texts.projects
+							.sort((project1, project2) => project1.id - project2.id)
+							.map((project) => (
+								<div
+									key={`div-${project.id}`}
+									className="project"
+									onClick={() => {
+										window.open(project.link, '_blank');
+									}}
+								>
+									<img
+										src={require(`../img/${project.name.replaceAll(
+											' ',
+											''
+										)}.png`)}
+										alt={project.name}
+									></img>
+									<div>
+										<h4 key={`title-${project.id}`}>{project.name}</h4>
+										<span key={`span-${project.id}`}>
+											{project.description}
+										</span>
+									</div>
+								</div>
+							))}
+					</div>
 				</section>
 			)}
 		</>
